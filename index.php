0a43caedbd53c6d69c5b55609b847af028328a70
Marko Marković <okram@civokram.com>
2013-01-23 00:07:49 +0100

    Rewrite of the 'engine'

--- index.php
+++ index.php
@@ -18,82 +18,38 @@ body {
 </head>
 <body>
 <?php
-/**
-* Scan current directory and prepare
-* array to hold list of files.
-*/
-$filesInCurrentFolder = scandir('.');
-$filteredFiles = array();
 
-/**
-* Valid:
-* 0.jpg
-* 01.jpg
-* 1.jpg
-* 10.jpg
-* 1 foo bar.jpg
-*
-* Invalid:
-* foo.jpg
-* 012 foo bar.jpg
-*/
-foreach ($filesInCurrentFolder as $file) {
-	if( preg_match( '/^([0-9]|[0-9][0-9]).*(\.jpe?g|\.png|\.gif)$/i', $file ) ) {
-		$filteredFiles[] = $file;
+// Read all the files in the current directory
+$allFiles = scandir('.');
+
+// Filter it so only images remain
+foreach ($allFiles as $key => $file)
+{
+	if (!preg_match('/.+\.(jpg|jpeg|png|gif)$/i', $file))
+	{
+		unset($allFiles[$key]);
 	}
 }
 
-/**
-* If there are no .jpg images
-* then just print error message
-*/
-if( empty( $filteredFiles ) ) {
-	echo '<h1>No images found!</h1>';
-} else {
-	/**
-	* Sort filtered files so this list:
-	* 1, 11, 12, 2, 3
-	* becomes this list:
-	* * 1, 2, 3, 11, 12
-	*/
-	sort( $filteredFiles, SORT_NUMERIC );
-
-	// get index of last element in filtered files array
-	$indexOfLastFilteredFile = key(array_slice($filteredFiles, -1, 1, TRUE));
-
-	/**
-	* Get URI and set requested image id
-	*/
-	$uri = $_SERVER["REQUEST_URI"];
-
-	$explodedUri = explode( '/', $uri );
-
-	$requestedImageId = $explodedUri[ key(array_slice( $explodedUri, -1, 1, TRUE)) ];
-
-	/**
-	* If requested image id matches one digit(without
-	* leading zero) or two digits AND if requested
-	* image id exists in $filteredFiles continue
-	* else show error message.
-	*/
-	if( preg_match( '/^([1-9]|[1-9][0-9])$/', $requestedImageId ) && array_key_exists($requestedImageId - 1, $filteredFiles) ) {
-		/**
-		* Check if requested file is actually last file
-		* and if so link last image to first image.
-		*/
-		if( $indexOfLastFilteredFile == $requestedImageId - 1 ) {
-			echo '<a href="1"><img src="' . $filteredFiles[$requestedImageId - 1] . '" alt=""></a>';
-		} else {
-			echo '<a href="' . ($requestedImageId + 1) . '"><img src="' . $filteredFiles[$requestedImageId - 1] . '" alt=""></a>';
-		}
-	} else {
-		if( $requestedImageId == '' ) {
-			echo '<a href="2"><img src="' . $filteredFiles[0] . '" alt=""></a>';
-		} else {
-			echo '<h1>ERROR!</h1>';
-		}
-	}
+// Check weather there are any images
+if (empty($allFiles))
+{
+	die('No images found.');
 }
+
+// Sort them 'naturally'
+sort($allFiles, SORT_NATURAL);
+
+// Determining current, next, and last file index. First is 1.
+$current = (isset($_GET['n']) && is_numeric($_GET['n'])) ? intval($_GET['n']) : 1;
+$last = count($allFiles);
+$next = ($current < $last) ? $current + 1 : 1; // Wrap-around
+
 ?>
+
+<a href="?n=<?php echo $next;?>">
+	<img src="<?php echo $allFiles[$current-1];?>" alt="<?php echo $allFiles[$current-1];?>">
+</a>
+
 </body>
 </html>
